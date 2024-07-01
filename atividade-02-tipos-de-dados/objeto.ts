7c0ae09190304738341d6fe680ff9a682dfe036c
Calistofolio <luanavenancio060@gmail.com>
2024-07-01 15:39:12 -0300

    boolea, interger e float, objeto, operadores logicos e string

---
+++ atividade-02-tipos-de-dados/objeto.ts
@@ -0,0 +1,12 @@
+//Questão 5
+type Animal = {
+    nome: string;
+    especie: string;
+    cor: string;
+}
+
+let cavalo: Animal{
+    nome = "Ponyta",
+    especie = "Cavalo",
+    cor = "Bege"
+}
---
+++ atividade-02-tipos-de-dados/string.ts
@@ -0,0 +1,21 @@
+//Questão 1
+let nome : string = "luana"
+console.log(nome)
+
+//Questão 2
+let frase1 : string = "Boy toy named Troy used to live in Detroit"
+console.log(frase1.length)
+
+//Questão 3
+let palavra : string = "vampiro"
+console.log(palavra.charAt(0))
+
+//Questão 4
+let frase2 : string = "The nose full of Tina Snow"
+console.log(frase2.toUpperCase())
+
+//Questão 5
+let endereco : string = "Edificio Porto Design"
+endereco = "Carpina, PE"
+console.log(endereco)
+
