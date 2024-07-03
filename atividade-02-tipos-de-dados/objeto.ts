a9d424ff595b397c73d1da11cf28bd57c603c1bc
Luana Calisto <luanavenancio060@gmail.com>
2024-07-02 21:22:54 -0300

    Atividade 02 finalizada

--- atividade-02-tipos-de-dados/objeto.ts
+++ atividade-02-tipos-de-dados/objeto.ts
@@ -1,3 +1,24 @@
+//Questão 1
+type Pessoa = {
+    nome: string;
+    idade: number;
+    endereco: string;
+}
+
+let pessoa1: Pessoa = {
+    nome: "Luana",
+    idade: 21,
+    endereco: "São Lourenço da Mata, PE",
+}
+
+//Questão 2
+console.log(pessoa1.nome)
+
+//Questão 3
+
+//Questão 4
+pessoa1.idade = 20
+
 //Questão 5
 type Animal = {
     nome: string;
@@ -5,8 +26,48 @@ type Animal = {
     cor: string;
 }
 
-let cavalo: Animal{
-    nome = "Ponyta",
-    especie = "Cavalo",
-    cor = "Bege"
+let animal1: Animal = {
+    nome: "Cavalo",
+    especie: "Cavalo",
+    cor: "Bege",
 }
+
+//Questão 6
+type Livro = {
+    titulo: string;
+    autor: string;
+    ano: number;
+}
+
+let livro1: Livro = {
+    titulo: "Dracula",
+    autor: "Bram Stoker",
+    ano: 1896,
+}
+
+//Questão 7
+console.log(livro1.autor)
+
+//Questão 8
+livro1.ano = 1897
+
+//Questão 9
+
+//Questão 10
+type Carro = {
+    marca: string;
+    modelo?: string;
+    ano: number;
+}
+
+let carro1: Carro = {
+    marca: "Hyundai",
+    modelo: "HB20",
+    ano: 2013,
+}
+
+console.log(carro1)
+
+//Questão 11
+carro1.modelo = undefined
+console.log(carro1)
---
+++ atividade-02-tipos-de-dados/typeOf.ts
@@ -0,0 +1,28 @@
+//Questão 1
+let numero: number = 5;
+console.log(typeof(numero))
+
+//Questão 2
+let texto: string = "Bridget Bardot";
+console.log(typeof(texto))
+
+//Questão 3
+let booleano: boolean = true;
+console.log(typeof(booleano))
+
+//Questão 4
+let array: string[]
+console.log(typeof(array))
+
+//Questão 5
+type Objeto = {}
+let objeto: Objeto = {}
+console.log(typeof(objeto))
+
+//Questão 6
+let nulo: null = null;
+console.log(typeof(nulo))
+
+//Questão 7
+let indefinido;
+console.log(typeof(indefinido))
